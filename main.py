81a187521abaeffdd20953f9e04c8af3b3dfb4dc
Aleksey Semenov <7.on.off@bk.ru>
2020-01-18 16:16:44 +0300

    Рефакторинг.
    Поменял способ тестового запуска функций.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tools.get_shop_list_by_dishes import get_shop_list_by_dishes
+from tools.parser import parser
 from pprint import pprint
 
-try:
-   result = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-except Exception as e:
-    print(f'Ошибка: {e}')
+with open('./recipes.txt', 'r', encoding='utf-8') as f:
+    print('Задание 1:')
+    pprint(parser(f))
+    print('Задание 2:')
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
