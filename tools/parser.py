2f67da5d0df7e31347921d2ab4ee1bd9936fcd5b
Aleksey Semenov <7.on.off@bk.ru>
2020-01-21 09:48:39 +0300

    Исправление.
    Меняет map и lambda функцию с неправильно написаным генератором словарей
    на более понятный цикл.

--- tools/parser.py
+++ tools/parser.py
@@ -5,8 +5,9 @@ def parser(file_data):
         next_recipe = int(data[1]) + 2
         recipe = data[:next_recipe:1]
         keys = ['ingredient_name', 'quantity', 'measure']
-        result[recipe[0]] = list(
-            map(lambda elem: {key: value for key, value in zip(keys, elem.split('|'))}, recipe[2:])
-        )
+        result[recipe[0]] = list()
+        for elem in recipe[2:]:
+            result[recipe[0]].append(
+                dict(zip(keys, elem.split('|'))))
         data = data[next_recipe + 1::1]
     return result
