00a5dfbf42b31f7066af55e932553e31b6fe2c76
Aleksey Semenov <7.on.off@bk.ru>
2020-01-21 10:04:13 +0300

    Исправление.
    Убирает лишниие параметры в срезах.

--- tools/parser.py
+++ tools/parser.py
@@ -3,11 +3,11 @@ def parser(file_data):
     result = {}
     while data:
         next_recipe = int(data[1]) + 2
-        recipe = data[:next_recipe:1]
+        recipe = data[:next_recipe]
         keys = ['ingredient_name', 'quantity', 'measure']
         result[recipe[0]] = list()
         for elem in recipe[2:]:
             result[recipe[0]].append(
                 dict(zip(keys, elem.split('|'))))
-        data = data[next_recipe + 1::1]
+        data = data[next_recipe + 1:]
     return result
